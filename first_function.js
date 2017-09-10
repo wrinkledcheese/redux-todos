1b2da1ee8d5498de70f23ca94da594e09346a0cd
WrinkledCheese <wrinkledcheese@gmail.com>
2017-09-10 20:37:38 -0300

    createStore and getState

--- first_function.js
+++ first_function.js
@@ -10,13 +10,13 @@ const function counter( state = 0, action ){
 		return state;
 }
 
-expect( counter( 0, { type: 'INCREMENT'} ) ).toEqual( 1 );
-expect( counter( 1, { type: 'INCREMENT'} ) ).toEqual( 2 );
-expect( counter( 0, { type: 'DECREMENT'} ) ).toEqual( 1 );
-expect( counter( 0, { type: 'DECREMENT'} ) ).toEqual( 0 );
-expect( counter( 1, { type: 'UNHANDLED_ACTION'} ) ).toEqual( 1 );
-expect( counter( undefined, {} ) ).toEqual( 0 );
+const { createStore } = Redux; /* ES6 */
+// var createStore = Redux.createStore; /*JS*/
+// import { createStore } from 'redux'; /*react + bable*/
 
-console.log( 'test passed' );
+const store = createStore( counter ); 
 
+console.log( sotre.getStore() );
 
+store.dispatch( { type: 'INCREMENT' } );
+console.log( store.getState() );
