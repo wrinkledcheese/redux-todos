3e7530abb59fb83c566377a4d3576aed7d69ae76
WrinkledCheese <wrinkledcheese@gmail.com>
2017-09-10 21:47:23 -0300

    reactify counter instead of modifying the dom directly

--- first_function.js
+++ first_function.js
@@ -12,9 +12,38 @@ const function counter( state = 0, action ){
 
 const { createStore } = Redux;
 
+const Counter = ( {
+	value,
+	onIncrement,
+	onDecrement
+
+}) => {
+	<div
+		<h1>{ value }</h1>
+		<button onClick={ onIncrement } >+</button>
+		<button onClick={ onDecrement } >-</button>
+};
+
 const store = createStore( counter ); 
 
-const render = () => { document.body.innerText = store.getState(); }; 
+const render = () => {
+	ReactDom.render(
+		<Counter value={ store.getState() }
+		onIncrement={ () => {
+			store.dispatch( {
+				type : 'INCREMENT', 
+			})
+		}
+		onDecrement={ () => {
+			store.dispatch( {
+				type : 'DECREMENT', 
+			})
+		}
+		 />,
+		}
+		document.getElementById( 'root' )
+	);
+}; 
 
 store.subscribe( render );
 render();
