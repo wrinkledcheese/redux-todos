458ba7d870bf2d4109a3c24d0debad56358857d8
Wayne Marsh <wayne@heimdallnetworks.com>
2017-09-13 16:22:29 -0300

    passing child context to children using provider component

--- todos.js
+++ todos.js
@@ -78,7 +78,7 @@ const Link = ({
 
 class FilterLink extends Component {
 	componenetDidMount(){
-		const { store } = this.props;
+		const { store } = this.context;
 		this.unsubscribe = store.subscribe(() =>
 			this.forceUpdate()
 		);
@@ -90,7 +90,7 @@ class FilterLink extends Component {
 
 	render() {
 		const props = this.props;
-		const { store } = props;
+		const { store } = this.context;
 		const state = store.getState();
 		return (
 			<Link 
@@ -110,6 +110,9 @@ class FilterLink extends Component {
 		);
 	}
 }
+FilterLink.contextTypes = {
+	store: React.PropTypes.object
+};
 
 const Todo = ( {
 	onClick,
@@ -144,7 +147,7 @@ const TodoList = ({
 	</ul>
 }
 
-const AddTodo = ({store}) => {
+const AddTodo = (props, { store }) => {
 	let input;
 
 	return (
@@ -166,20 +169,23 @@ const AddTodo = ({store}) => {
 	);
 
 }
+AddTodo.contextTypes = {
+	store: React.PropTypes.object
+};
 
-const Footer = ({store}) => {
+const Footer = () => {
 	<p>
 		Show:
 		{ ' ' }
-		<FilterLink filter='SHOW_ALL' store={store}>
+		<FilterLink filter='SHOW_ALL'>
 			All
 		</FilterLink>
 		{ ', ' }
-		<FilterLink filter='SHOW_ACTIVE' store={store}>
+		<FilterLink filter='SHOW_ACTIVE'>
 			Active
 		</FilterLink>
 		{ ', ' }
-		<FilterLink filter='SHOW_COMPLETED' store={store}>
+		<FilterLink filter='SHOW_COMPLETED'>
 			Completed
 		</FilterLink>
 	</p>
@@ -205,7 +211,7 @@ const getVisibleTodos = (
 
 class VisibleTodoList extends Component {
 	componenetDidMount(){
-		const { store } = this.props;
+		const { store } = this.context;
 		this.unsubscribe = store.subscribe(() =>
 			this.forceUpdate()
 		);
@@ -217,7 +223,7 @@ class VisibleTodoList extends Component {
 
 	render() {
 		const props = this.props;
-		const { store } = props
+		const { store } = this.context;
 		const state = store.getState();
 
 		return (
@@ -238,24 +244,43 @@ class VisibleTodoList extends Component {
 		);	
 	}
 }
+VisibleTodoList.contextTypes = {
+	store: React.PropTypes.object
+};
 
 let nextTodoId = 0;
-const TodoApp = ({store}) => (
+const TodoApp = () => (
 		
 	const visibleTodos = getVisibleTodos(
 		todos,
 		visibilityFilter
 	);
 	<div>
-		<AddTodo store={store} />
-		<VisibleTodoList store={store} />
-		<Footer store={store} />
+		<AddTodo />
+		<VisibleTodoList />
+		<Footer />
 	</div>
 );
+
+class Provider extends Component {
+	getChildContext(){
+		return {
+			store: this.props.children
+		};
+	}
+	render(){
+		return this.props.children
+	};
+}
+Provider.childContextTypes = {
+	store: React.PropTypes.object
+};
 
 const { createStore } = Redux;
 
 ReactDOM.render( 
-	<TodoApp store={createStore( todoApp )} />,
+	<Provider store={createStore( todoApp ) }>
+		<TodoApp />
+	</Provider>,
 	document.getElementById( 'root' )
 );
