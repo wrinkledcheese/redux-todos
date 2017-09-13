bcb9dfa189f47cf3cf86340e8f1b2d87256c21d3
WrinkledCheese <wrinkledcheese@gmail.com>
2017-09-13 06:28:25 -0300

    implementing combineReducers from scratch

--- todos.js
+++ todos.js
@@ -45,7 +45,20 @@ const visibilityFilter = (
 	}
 };
 
-const { combineReducers } = Redux;
+const combineReducers = ( reducers ) => {
+	return ( state = {}, action ) ={
+		return Object.keys( reducers ).reduce( 
+			( nextState, key ) => {
+				nextState[ key ] = reducers[ key ]( 
+					state[ key ],
+					action
+				);
+				return nextState;
+			},
+			{}
+		});
+	};
+};
 const todoApp = combineReducers( {
 	//ES6 object literal shorthand notation
 	todos,
