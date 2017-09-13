7b54395f32ab784dff2a618dd577c4a81277b4b4
WrinkledCheese <wrinkledcheese@gmail.com>
2017-09-12 21:19:10 -0300

    todos add todo reducer

--- todos.js
+++ todos.js
@@ -1,26 +1,43 @@
-const toggleTodo = ( todo ) => {
-	return {
-		...todo, 
-		completed : !todo.completed
-	};
+const todos = ( state = [], action ) => {
+	switch( action.type ){
+		case 'ADD_TODO':
+			return [
+				...state,
+				{
+					id: action.id,
+					text: action.text,
+					completed: false
+				}
+			];
+		default:
+			return state;
+	}
 };
 
-const testToggleTodo = () => {
-	const todoBefore = {
-		id = 0,
-		text = 'Leard Redux',
-		completed = false
-	};
-	const todoAfter = {
-		id = 0,
-		text = 'Leard Redux',
-		completed = true
-	};
+const testAddTodo = () => {
+	const stateBefore = [];
+	const action = {
+		type : 'ADD_TODO',
+		id: 0,
+		text: 'Learn Redux'
 
-	expect(
-		toggleTodo( todoBefore )
-	).toEqual( todoAfter );
+	};
+	const stateAfter = [
+		{
+			id: 0,
+			text: 'Learn Redux',
+			completed: false
+		}
+	];
+
+	deepFreeze( stateBefore );
+	deepFreeze( action );
+
+	expect( 
+		todos( stateBefore, action ) 
+	).toEqual( stateAfter );
+
 };
 
-testToggleTodo();
+testAddTodo();
 console.log( 'All tests passed.' );
