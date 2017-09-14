6591023d71ab0f20817cb7228576f9416929f899
Wrinkled Cheese <wrinkledcheese@gmail.com>
2017-09-14 17:47:15 -0300

    treat the state as a database using combineReducers, selectors, and experimental object spread operator

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,53 +1,57 @@
-const todo = ( state , action ) => {
-	switch( action.type ) {
-		case 'ADD_TODO':
-			return {
-				id: action.id,
-				text: action.text,
-				completed: false
-			};
-		case 'TOGGLE_TODO':
-			if( state.id !== action.id ) {
-				return state;
-			}
-			return {
-				...state,
-				completed: !state.completed
-				};
-		default:
-			return state;
-	}
-};
+import { combineReducers } from 'redux';
+import todo from './todo';
 
-const todos = ( state = [], action ) => {
+const byId = ( state = {} , action ) => {
 	switch( action.type ){
 		case 'ADD_TODO':
-			return [
-				...state,
-				todo( undefined, action )
-			];
 		case 'TOGGLE_TODO':
-			returd state.map( t => todo( t, action ) );
+			return {
+				/*
+				object spread requires babelrc to have "plugins": ["transform-object-rest-spread"]
+				object spread also requires bable plugin: "devDependencies":{ "babel-plugin-transform-object-rest-spread":"^6.6.5"
+				*/
+				...state,
+				[ action.id ] todo( state[ action.id ], action )
+			};
 		default:
 			return state;
 	}
 };
 
+const allIds = = ( state = [], action ) => {
+	switch ( action.type ) {
+		case 'ADD_TODO':
+			return [ ...state, action.id ];
+		default:
+			return state;
+	}
+}
+
+
+const todos = combineReducers( {
+	byId,
+	allIds
+});
+
 export default todos;
+
+const getAllTodos = ( state ) => 
+	state.allIds.map( id => state.byId[ id ]);
 
 export const getVisibleTodos = (
 	state,
 	filter
 ) => {
+	const allTodos = getAllTodos( state );
 	switch ( filter ) {
 		case 'all':
-			return state;
+			return allTodos;
 		case 'completed':
-			return state.filter( 
+			return allTodos.filter( 
 				t => t.completed 
 			);
 		case 'active':
-			return state.filter( 
+			return allTodos.filter( 
 				t => !t.completed 
 			);
 		default:
